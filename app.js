dfcce43ad46bae9d9bde850cb22760b4524198ad
urnmo <aaron0moore@gmail.com>
2016-10-05 22:24:36 -0400

    added more controllers

--- app.js
+++ app.js
@@ -3,31 +3,44 @@ let app = angular.module("bookLendery", []);
 
 
 
-app.controller("checkOut", function ($scope){
- 
- $scope.books = [ {book:"Thirty Days in the Samarkind Desert with the Duchess of Kent"}, {book:"A Hundred and One Ways to Start a Fight"},  {book:"Biggles Combs his Hair"}, {book:'The Gospel According to Charley Drake'}, {book:"Ethel the Aardvark goes Quantity Surveying"}, {book:"The Amazing Adventures of Captain Gladys Stoutpamphlet and her Intrepid Spaniel Stig Amongst the Giant Pygmies of Beckles"}];
-// write the function that happens on button click
+app.controller("checkOut", function ($scope, CheckedOutBooks) {
+    $scope.books = CheckedOutBooks.get();
+    $scope.checkOutButt = function (book) {
+        CheckedOutBooks.borrow(book, $scope.name);
+    }
 });
 
-$scope.checkOutButt = function(){
 
-},
-
-app.factory("CheckedOutBooks", function(){
+app.factory("CheckedOutBooks", function () {
     let borrowed = [];
+    let books = [{ book: "Thirty Days in the Samarkind Desert with the Duchess of Kent", author: "Fanny Brice", borrower: "", }, { book: "A Hundred and One Ways to Start a Fight", author: "Some Irish Gentleman", borrower: "", }, { book: "Biggles Combs his Hair", author: "Sam E.J. Elliot OBE", borrower: "", }, { book: 'The Gospel According to Charley Drake', author: "Can Newton", borrower: "", }, { book: "Ethel the Aardvark goes Quantity Surveying", author: "Kristy Fletcher", borrower: "", }, { book: "The Amazing Adventures of Captain Gladys Stoutpamphlet and her Intrepid Spaniel Stig Amongst the Giant Pygmies of Beckles", author: "Jeb Cletus", borrower: "", }];
+    // write the function that happens on button click
 
     return {
-        add: function (book, borrower){
-            borrowed.push({
-                book: book,
-                borrower: borrower,
-            });
+
+        get: function () {
+            return books;
         },
-        all: function(){
-            return borrowed;
+
+        checkOut: function (titleOfBook, username) {
+            for (let i = 0; i < books.length; i++) {
+                if (books[i].title === titleOfBook) {
+                    books[i].borrower = username;
+                }
+            }
         },
-    };
+
+        borrowed: function (person) {
+            let outList = [];
+            for (let i = 0; i < books.length; i++) {
+                if (user === books[i].borrower) {
+                    outList.push(books[i]);
+                }
+            }
+            console.log(outlist);
+            return outList;
+        }
+    }
 });
 
 
-// app.controller("lent", function ($scope, CheckedOutBooks,){});
