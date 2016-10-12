f17b057cea51f5af23fff4fa2d671d7489154b57
urnmo <aaron0moore@gmail.com>
2016-10-12 11:23:28 -0400

    functionality added to buttons

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ let app = angular.module("bookLendery", []);
 
 app.controller("checkOut", function ($scope, CheckedOutBooks) {
     $scope.books = CheckedOutBooks.get();
-    $scope.checkOutButt = function (book) {
-        CheckedOutBooks.borrow(book, $scope.name);
+    $scope.checkout = function (thisBook) {
+        CheckedOutBooks.checkOut(thisBook, $scope.name);
     }
 });
 
+app.controller("lent", function ($scope, CheckedOutBooks) {
+   
+    
+});
 
 app.factory("CheckedOutBooks", function () {
     let borrowed = [];
-    let books = [{ book: "Thirty Days in the Samarkind Desert with the Duchess of Kent", author: "Fanny Brice", borrower: "", }, { book: "A Hundred and One Ways to Start a Fight", author: "Some Irish Gentleman", borrower: "", }, { book: "Biggles Combs his Hair", author: "Sam E.J. Elliot OBE", borrower: "", }, { book: 'The Gospel According to Charley Drake', author: "Can Newton", borrower: "", }, { book: "Ethel the Aardvark goes Quantity Surveying", author: "Kristy Fletcher", borrower: "", }, { book: "The Amazing Adventures of Captain Gladys Stoutpamphlet and her Intrepid Spaniel Stig Amongst the Giant Pygmies of Beckles", author: "Jeb Cletus", borrower: "", }];
+    let books = [{ book: "Thirty Days in the Samarkind Desert with the Duchess of Kent", author: "Fanny Brice", borrower: "none", }, { book: "A Hundred and One Ways to Start a Fight", author: "Some Irish Gentleman", borrower: "none", }, { book: "Biggles Combs his Hair", author: "Sam E.J. Elliot OBE", borrower: "none", }, { book: 'The Gospel According to Charley Drake', author: "Can Newton", borrower: "none", }, { book: "Ethel the Aardvark goes Quantity Surveying", author: "Kristy Fletcher", borrower: "none", }, { book: "The Amazing Adventures of Captain Gladys Stoutpamphlet and her Intrepid Spaniel Stig Amongst the Giant Pygmies of Beckles", author: "Jeb Cletus", borrower: "none", }];
     // write the function that happens on button click
 
     return {
@@ -22,12 +26,13 @@ app.factory("CheckedOutBooks", function () {
             return books;
         },
 
-        checkOut: function (titleOfBook, username) {
+        checkOut: function (book, borrower) {
             for (let i = 0; i < books.length; i++) {
-                if (books[i].title === titleOfBook) {
-                    books[i].borrower = username;
+                if (books[i].book === book) {
+                    books[i].borrower = borrower;
                 }
             }
+            console.log("hello");
         },
 
         borrowed: function (person) {
